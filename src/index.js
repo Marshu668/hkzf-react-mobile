ec09aad719ea0e165b6e3ab54cc82a1fbb081f86
Marshu668 <869715017@qq.com>
2020-03-08 16:38:54 +0800

    初始化项目

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// 导入antd-mobile样式
+import 'antd-mobile/dist/antd-mobile.css'
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
